07bc115c5b48f604e4e59ec70f0691a1ecfaeb4d
benjacobson <bentjacobson@gmail.com>
2017-10-29 22:01:03 -0600

    break apart main app functions

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 class App {
 	constructor() {
+		this.camara = new Camara(0, 0, -50, 0, 0);
+		this.keysPressed = {};
+		this.mouseX = 0;
+		this.mouseY = 0;
+		this.TAU = Math.PI * 2;
+		this.translateFactor = 15;
+		this.rotateFactor = 1000;
+		this.translateSpeed = 0.7;
+		this.rotateSpeed = 0.03;
+		this.mouseLook = false;
+		this.timeBased = false;
+
+		this.viewMatrix = new Float32Array(16);
+		this.projMatrix = new Float32Array(16);
+
 		this.loadAssets();
 	}
 
@@ -25,19 +40,120 @@ class App {
 	}
 
 	start() {
-		this.assetsLoaded.then(this.init.bind(this));
+		this.assetsLoaded.then(() => this.init());
+	}
+
+	draw() {
+		this.gl.clear(this.gl.DEPTH_BUFFER_BIT | this.gl.COLOR_BUFFER_BIT);
+		let sunRotx = (performance.now()/1333) % this.TAU;
+		let sunRoty = (performance.now()/917) % this.TAU;
+		let sunRotz = (performance.now()/577) % this.TAU;
+		let sunPoint = [Math.sin(sunRotx)*15.0, Math.sin(sunRoty)*15.0, Math.sin(sunRotz)*15.0];
+		this.textureAndLightProgramInfo.use();
+		// Blocks
+		this.gl.bindVertexArray(this.blockMeshSet.vao);
+		this.textureAndLightProgramInfo.setLightPoint(sunPoint);
+		this.blockMeshSet.bindTextures();
+		this.blockMeshSet.draw();
+		// Light
+		this.gl.bindVertexArray(this.lightMeshSet.vao);
+		this.lightMeshSet.updateVertexData([new Block(...sunPoint)]);
+		this.lightMeshSet.bindTextures();
+		this.lightMeshSet.draw();
+		// // bunny
+		this.lightProgramInfo.use();
+		this.lightProgramInfo.setLightPoint(sunPoint);
+		this.gl.bindVertexArray(this.bunnyMeshSet.vao);
+		this.bunnyMeshSet.draw();
+		// sphere
+		this.gl.bindVertexArray(this.sphereMeshSet.vao);
+		this.sphereMeshSet.draw();
+	}
+
+	processMovement() {
+		let now = performance.now();
+		if (this.keysPressed['w']) {
+			let speed =  (now - this.keysPressed['w']) / this.translateFactor;
+			this.keysPressed['w'] = now;
+			this.camara.moveForward(this.timeBased ? speed : this.translateSpeed);
+		}
+		if (this.keysPressed['s']) {
+			let speed = (now - this.keysPressed['s']) / this.translateFactor;
+			this.keysPressed['s'] = now;
+			this.camara.moveBackward(this.timeBased ? speed : this.translateSpeed);
+		}
+		if (this.keysPressed['a']) {
+			let speed = (now - this.keysPressed['a']) / this.translateFactor;
+			this.keysPressed['a'] = now;
+			this.camara.moveLeft(this.timeBased ? speed : this.translateSpeed);
+		}
+		if (this.keysPressed['d']) {
+			let speed = (now - this.keysPressed['d']) / this.translateFactor;
+			this.keysPressed['d'] = now;
+			this.camara.moveRight(this.timeBased ? speed : this.translateSpeed);
+		}
+		if (this.keysPressed['ArrowLeft']) {
+			let speed = (now - this.keysPressed['ArrowLeft']) / this.rotateFactor;
+			this.keysPressed['ArrowLeft'] = now;
+			this.camara.rotateLeft(this.timeBased ? speed : this.rotateSpeed);
+		}
+		if (this.keysPressed['ArrowRight']) {
+			let speed = (now - this.keysPressed['ArrowRight']) / this.rotateFactor;
+			this.keysPressed['ArrowRight'] = now;
+			this.camara.rotateRight(this.timeBased ? speed : this.rotateSpeed);
+		}
+		if (this.keysPressed['ArrowUp']) {
+			let speed = (now - this.keysPressed['ArrowUp']) / this.rotateFactor;
+			this.keysPressed['ArrowUp'] = now;
+			this.camara.rotateForward(this.timeBased ? speed : this.rotateSpeed);
+		}
+		if (this.keysPressed['ArrowDown']) {
+			let speed = (now - this.keysPressed['ArrowDown']) / this.rotateFactor;
+			this.keysPressed['ArrowDown'] = now;
+			this.camara.rotateBackward(this.timeBased ? speed : this.rotateSpeed);
+		}
+		if (this.mouseLook) {
+			this.camara.rotx = this.mouseX * this.rotateFactor;
+			this.camara.roty += this.mouseY;
+			this.camara.change = true;
+		}
+		if (this.camara.change) {
+			if (this.camara.y > -1.0) {
+				this.camara.y = -1.0;
+			}
+			mat4.identity(this.viewMatrix);
+			mat4.rotateX(this.viewMatrix, this.viewMatrix, this.camara.rotx);
+			mat4.rotateY(this.viewMatrix, this.viewMatrix, this.camara.roty);
+			mat4.translate(this.viewMatrix, this.viewMatrix, this.camara.getLocation());
+			this.textureAndLightProgramInfo.use();
+			this.textureAndLightProgramInfo.setViewMatrix(this.viewMatrix);
+			this.lightProgramInfo.use();
+			this.lightProgramInfo.setViewMatrix(this.viewMatrix);
+			this.camara.change = false;
+		}
+	}
+
+	resizeHandler() {
+		this.canvas.width = window.innerWidth;
+		this.canvas.height = window.innerHeight;
+		this.gl.viewport(0, 0, window.innerWidth, window.innerHeight);
+		mat4.perspective(this.projMatrix, glMatrix.toRadian(45), window.innerWidth / window.innerHeight, 1.0, 5000.0);
+		this.textureAndLightProgramInfo.use();
+		this.textureAndLightProgramInfo.setProjMatrix(this.projMatrix);
+		this.lightProgramInfo.use();
+		this.lightProgramInfo.setProjMatrix(this.projMatrix);
 	}
 
 	init() {
-		let canvas = document.getElementById('glCanvas');
-		let gl = canvas.getContext('webgl2');
+		this.canvas = document.getElementById('glCanvas');
+		this.gl = this.canvas.getContext('webgl2');
 
-		if (!gl) {
+		if (!this.gl) {
 			alert('Your browser does not support WebGL2');
 			return;
 		}
 
-		let textureAndLightProgramInfo = new TextureAndLightProgramInfo(gl, this.assetMap.get(this.textureAndLightVert), this.assetMap.get(this.textureAndLightFrag));
+		this.textureAndLightProgramInfo = new TextureAndLightProgramInfo(this.gl, this.assetMap.get(this.textureAndLightVert), this.assetMap.get(this.textureAndLightFrag));
 
 		let blockMeshes = [new Block(10, -2.55, 10), new Block(-10, -2.55, 10), new Block(10, -2.55, -10), new Block(-10, -2.55, -10)];
 		for (let i = 0; i < 1000; i++) {
@@ -46,7 +162,7 @@ class App {
 			let z = Math.floor(Math.random()*1000) - 500;
 			blockMeshes.push(new Block(x, y, z));
 		}
-		let blockMeshSet = new MeshSet(gl, blockMeshes, textureAndLightProgramInfo,
+		this.blockMeshSet = new MeshSet(this.gl, blockMeshes, this.textureAndLightProgramInfo,
 			[
 				new GLSLAttribute('vertCoord', 'vertexData', 'vertexComponents'),
 				new GLSLAttribute('normal', 'normalData', 'normalComponents'),
@@ -58,7 +174,7 @@ class App {
 		);
 
 		let lightMeshes = [new Block(0, 0, 0, true)];
-		let lightMeshSet = new MeshSet(gl, lightMeshes, textureAndLightProgramInfo,
+		this.lightMeshSet = new MeshSet(this.gl, lightMeshes, this.textureAndLightProgramInfo,
 			[
 				new GLSLAttribute('vertCoord', 'vertexData', 'vertexComponents'),
 				new GLSLAttribute('normal', 'normalData', 'normalComponents'),
@@ -70,9 +186,9 @@ class App {
 		);
 
 
-		let lightProgramInfo = new LightProgramInfo(gl, this.assetMap.get(this.lightVert), this.assetMap.get(this.lightFrag));
+		this.lightProgramInfo = new LightProgramInfo(this.gl, this.assetMap.get(this.lightVert), this.assetMap.get(this.lightFrag));
 		let bunnyMeshes = [new Bunny(0, 10, 0)];
-		let bunnyMeshSet = new MeshSet(gl, bunnyMeshes, lightProgramInfo,
+		this.bunnyMeshSet = new MeshSet(this.gl, bunnyMeshes, this.lightProgramInfo,
 			[
 				new GLSLAttribute('vertCoord', 'vertexData', 'vertexComponents'),
 				new GLSLAttribute('normal', 'normalData', 'normalComponents'),
@@ -81,7 +197,7 @@ class App {
 		);
 
 		let sphereMeshes = [new Sphere(20, -10, 0, 0), new Sphere(20, 0, 0, 0), new Sphere(20, 10, 0, 0)];
-		let sphereMeshSet = new MeshSet(gl, sphereMeshes, lightProgramInfo,
+		this.sphereMeshSet = new MeshSet(this.gl, sphereMeshes, this.lightProgramInfo,
 			[
 				new GLSLAttribute('vertCoord', 'vertexData', 'vertexComponents'),
 				new GLSLAttribute('normal', 'normalData', 'normalComponents'),
@@ -90,192 +206,67 @@ class App {
 		);
 
 		// gl.clearColor(0.8, 0.9, 1.0, 1.0); // light blue
-		gl.clearColor(0.0, 0.0, 0.0, 1.0); // black
-		gl.enable(gl.DEPTH_TEST);
-		gl.depthFunc(gl.LEQUAL);
+		this.gl.clearColor(0.0, 0.0, 0.0, 1.0); // black
+		this.gl.enable(this.gl.DEPTH_TEST);
+		this.gl.depthFunc(this.gl.LEQUAL);
 
-		let viewMatrix = new Float32Array(16);
-		let projMatrix = new Float32Array(16);
-		mat4.identity(viewMatrix);
-		mat4.perspective(projMatrix, glMatrix.toRadian(45), canvas.clientWidth / canvas.clientHeight, 1.0, 5000.0);
+		mat4.identity(this.viewMatrix);
+		mat4.perspective(this.projMatrix, glMatrix.toRadian(45), this.canvas.clientWidth / this.canvas.clientHeight, 1.0, 5000.0);
 
-		textureAndLightProgramInfo.use();
-		textureAndLightProgramInfo.setViewMatrix(viewMatrix);
-		textureAndLightProgramInfo.setProjMatrix(projMatrix);
-		lightProgramInfo.use();
-		lightProgramInfo.setViewMatrix(viewMatrix);
-		lightProgramInfo.setProjMatrix(projMatrix);
+		this.textureAndLightProgramInfo.use();
+		this.textureAndLightProgramInfo.setViewMatrix(this.viewMatrix);
+		this.textureAndLightProgramInfo.setProjMatrix(this.projMatrix);
+		this.lightProgramInfo.use();
+		this.lightProgramInfo.setViewMatrix(this.viewMatrix);
+		this.lightProgramInfo.setProjMatrix(this.projMatrix);
 
-		//
-		// resize handler
-		//
-		function resize() {
-			canvas.width = window.innerWidth;
-			canvas.height = window.innerHeight;
-			gl.viewport(0, 0, window.innerWidth, window.innerHeight);
-			mat4.perspective(projMatrix, glMatrix.toRadian(45), window.innerWidth / window.innerHeight, 1.0, 5000.0);
-			textureAndLightProgramInfo.use();
-			textureAndLightProgramInfo.setProjMatrix(projMatrix);
-			lightProgramInfo.use();
-			lightProgramInfo.setProjMatrix(projMatrix);
-		}
-		document.body.onresize = resize;
-		resize();
-
-		//
-		// state varialbes
-		//
-		let keysPressed = {};
-		let camara = new Camara(0, 0, -50, 0, 0);
-		let mouseX = 0;
-		let mouseY = 0;
-		let TAU = Math.PI * 2;
-		let translateFactor = 15;
-		let rotateFactor = 1000;
-		let translateSpeed = 0.7;
-		let rotateSpeed = 0.03;
-		let mouseLook = false;
-		let timeBased = false;
+		document.body.onresize = this.resizeHandler.bind(this);
+		this.resizeHandler();
 
 		//
 		// key handler
 		//
 		document.body.onkeydown = (event) => {
-			keysPressed[event.key] = performance.now();
+			this.keysPressed[event.key] = performance.now();
 		};
 
 		document.body.onkeyup = (event) => {
-			keysPressed[event.key] = undefined;
+			this.keysPressed[event.key] = undefined;
 		};
 
 		//
 		// mouse handler
 		//
 		document.body.onmousemove = (event) => {
-			mouseX = 2 * rotateSpeed * event.clientY / canvas.clientHeight - rotateSpeed;
-			mouseY = 2 * rotateSpeed * event.clientX / canvas.clientWidth - rotateSpeed;
-			mouseX = 10 * Math.abs(mouseX) * mouseX;
-			if (Math.abs(mouseY) < 0.0001) mouseY = 0;
+			this.mouseX = 2 * this.rotateSpeed * event.clientY / this.canvas.clientHeight - this.rotateSpeed;
+			this.mouseY = 2 * this.rotateSpeed * event.clientX / this.canvas.clientWidth - this.rotateSpeed;
+			this.mouseX = 10 * Math.abs(this.mouseX) * this.mouseX;
+			if (Math.abs(this.mouseY) < 0.0001) this.mouseY = 0;
 		};
-
-		//
-		// movement
-		//
-		function move() {
-			let now = performance.now();
-			if (keysPressed['w']) {
-				let speed =  (now - keysPressed['w']) / translateFactor;
-				keysPressed['w'] = now;
-				camara.moveForward(timeBased ? speed : translateSpeed);
-			}
-			if (keysPressed['s']) {
-				let speed = (now - keysPressed['s']) / translateFactor;
-				keysPressed['s'] = now;
-				camara.moveBackward(timeBased ? speed : translateSpeed);
-			}
-			if (keysPressed['a']) {
-				let speed = (now - keysPressed['a']) / translateFactor;
-				keysPressed['a'] = now;
-				camara.moveLeft(timeBased ? speed : translateSpeed);
-			}
-			if (keysPressed['d']) {
-				let speed = (now - keysPressed['d']) / translateFactor;
-				keysPressed['d'] = now;
-				camara.moveRight(timeBased ? speed : translateSpeed);
-			}
-			if (keysPressed['ArrowLeft']) {
-				let speed = (now - keysPressed['ArrowLeft']) / rotateFactor;
-				keysPressed['ArrowLeft'] = now;
-				camara.rotateLeft(timeBased ? speed : rotateSpeed);
-			}
-			if (keysPressed['ArrowRight']) {
-				let speed = (now - keysPressed['ArrowRight']) / rotateFactor;
-				keysPressed['ArrowRight'] = now;
-				camara.rotateRight(timeBased ? speed : rotateSpeed);
-			}
-			if (keysPressed['ArrowUp']) {
-				let speed = (now - keysPressed['ArrowUp']) / rotateFactor;
-				keysPressed['ArrowUp'] = now;
-				camara.rotateForward(timeBased ? speed : rotateSpeed);
-			}
-			if (keysPressed['ArrowDown']) {
-				let speed = (now - keysPressed['ArrowDown']) / rotateFactor;
-				keysPressed['ArrowDown'] = now;
-				camara.rotateBackward(timeBased ? speed : rotateSpeed);
-			}
-			if (mouseLook) {
-				camara.rotx = mouseX * rotateFactor;
-				camara.roty += mouseY;
-				camara.change = true;
-			}
-			if (camara.change) {
-				if (camara.y > -1.0) {
-					camara.y = -1.0;
-				}
-				mat4.identity(viewMatrix);
-				mat4.rotateX(viewMatrix, viewMatrix, camara.rotx);
-				mat4.rotateY(viewMatrix, viewMatrix, camara.roty);
-				mat4.translate(viewMatrix, viewMatrix, camara.getLocation());
-				textureAndLightProgramInfo.use();
-				textureAndLightProgramInfo.setViewMatrix(viewMatrix);
-				lightProgramInfo.use();
-				lightProgramInfo.setViewMatrix(viewMatrix);
-				camara.change = false;
-			}
-		}
-
-		//
-		// drawing
-		//
-		function draw() {
-			gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
-			let sunRotx = (performance.now()/1333) % TAU;
-			let sunRoty = (performance.now()/917) % TAU;
-			let sunRotz = (performance.now()/577) % TAU;
-			let sunPoint = [Math.sin(sunRotx)*15.0, Math.sin(sunRoty)*15.0, Math.sin(sunRotz)*15.0];
-			textureAndLightProgramInfo.use();
-			// Blocks
-			gl.bindVertexArray(blockMeshSet.vao);
-			textureAndLightProgramInfo.setLightPoint(sunPoint);
-			blockMeshSet.bindTextures();
-			blockMeshSet.draw();
-			// Light
-			gl.bindVertexArray(lightMeshSet.vao);
-			lightMeshSet.updateVertexData([new Block(...sunPoint)]);
-			lightMeshSet.bindTextures();
-			lightMeshSet.draw();
-			// // bunny
-			lightProgramInfo.use();
-			lightProgramInfo.setLightPoint(sunPoint);
-			gl.bindVertexArray(bunnyMeshSet.vao);
-			bunnyMeshSet.draw();
-			// sphere
-			gl.bindVertexArray(sphereMeshSet.vao);
-			sphereMeshSet.draw();
-		}
+		
 
 		//
 		// Main render loop
 		//
 		function loop() {
-			requestAnimationFrame(loop);
-			move();
-			draw();
+			requestAnimationFrame(loop.bind(this));
+			this.processMovement();
+			this.draw();
 		}
-		requestAnimationFrame(loop);
+		requestAnimationFrame(loop.bind(this));
 
 		//
 		// settings events
 		//
 		document.getElementById('mouseLook').addEventListener('change', function(e) {
-			mouseLook = e.target.checked;
-			if (!mouseLook) {
-				rotx = 0;
-				change = true;
+			this.mouseLook = e.target.checked;
+			if (!this.mouseLook) {
+				this.camara.rotx = 0;
+				this.camara.change = true;
 			}
 		});
 		document.getElementById('timeBased').addEventListener('change', function(e) {
-			timeBased = e.target.checked;
+			this.timeBased = e.target.checked;
 		});
 	}
 }
